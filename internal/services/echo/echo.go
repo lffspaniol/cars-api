bc14a26c6cac1503c2bd9f77f608922c07b2b38b
luis fernando fontoura spaniol <luisspaniol12@gmail.com>
2023-09-11 21:35:22 -0300

    Initial commit

---
+++ internal/services/echo/echo.go
@@ -0,0 +1,36 @@
+package echo
+
+import (
+	"context"
+
+	"go.uber.org/zap"
+)
+
+//go:generate mockgen -destination=mock/mock_service.go -package=mock boilerplate/internal/services/echo Service
+type Service interface {
+	Healthcheck(context.Context) error
+	Echo(ctx context.Context, message string) (string, error)
+}
+
+var _ Service = (*service)(nil)
+
+type service struct {
+	log *zap.Logger
+}
+
+func (s *service) Healthcheck(context.Context) error {
+	// dependencies healthcheck
+	s.log.Info("echo service healthcheck")
+	return nil
+}
+
+func (s *service) Echo(_ context.Context, message string) (string, error) {
+	s.log.Info("echo service echo", zap.String("message", message))
+	return message, nil
+}
+
+func New(log *zap.Logger) Service {
+	return &service{
+		log: log,
+	}
+}
---
+++ internal/services/healthcheck/healthcheck.go
@@ -0,0 +1,52 @@
+package healthcheck
+
+import (
+	"context"
+	"errors"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/codes"
+	"golang.org/x/sync/errgroup"
+)
+
+const pkgName = "healthcheck"
+const OK = "OK"
+
+var ErrDepencenciesFailed = errors.New("depencencies failed")
+
+//go:generate mockgen -source=healthcheck.go -destination=mock/healthcheck.go -package=mock
+type Depencencie interface {
+	Healthcheck(context.Context) error
+}
+
+type Alive struct {
+	depencencies []Depencencie
+}
+
+func (alive *Alive) Readiness(ctx context.Context) error {
+	ctx, span := otel.Tracer(pkgName).Start(ctx, "Readiness")
+	defer span.End()
+
+	g, ctx := errgroup.WithContext(ctx)
+
+	for _, depencency := range alive.depencencies {
+		depencency := depencency
+		g.Go(func() error {
+			return depencency.Healthcheck(ctx)
+		})
+	}
+
+	if err := g.Wait(); err != nil {
+		span.RecordError(err)
+		span.SetStatus(codes.Error, err.Error())
+		return errors.Join(ErrDepencenciesFailed, err)
+	}
+
+	return nil
+}
+
+func New(depencencies ...Depencencie) Alive {
+	return Alive{
+		depencencies: depencencies,
+	}
+}
